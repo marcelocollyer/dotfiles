89e1b0bb5a2d40b972f1babf8e5731c4024c1d8f
Marcelo Collyer <marcelocollyer@gmail.com>
2021-07-15 07:35:18 -0300

    Replacing gimp by seashore and adding turbo boost switcher

--- osx-system-defaults.sh
+++ osx-system-defaults.sh
@@ -1,1 +1,10 @@
 #!/bin/zsh
+echo "\nSet OsX system defaults"
+# ==============================================
+# Set Input preferences
+# ==============================================
+# Disable auto-correct
+ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Set scroll direction
+defaults write /Library/Preferences/.GlobalPreferences com.apple.swipescrolldirection -bool false
